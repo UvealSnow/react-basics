1070011a686c7518cecd5b8bed8d116414c9416d
Kevin Avila <kevin.avila@avantgarde.de>
2022-02-15 09:46:14 +0100

    What's JSX?

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  /**
-   *  Creates an element with the desired characteristics.
-   * 
-   * @param type keyof React.ReactHTML
-   * @param props? React.ReactClassAttributes
-   * @param ...children? React.ReactNode
-   */
-  React.createElement(
-    "h1",
-    { style: { color: 'blue' } },
-    "Hello World!"
-  ),
+  // This is using JSX (JavaScript as XML) this uses Babel to make this work.
+  //https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&corejs=3.6&spec=false&loose=false&code_lz=EoUwhgxgLgIg8gWQHQCcQDsAmIUAoBQABIQPQmEAqAFgJYDOh9hArnTegOaEBSAygBqFc3MADcwvCChoAHKITAN-CADIBKQlFoNWIBgCEwAIxAAbTQHtCAWzABrEJu2EA7hZR2kRQgB5mpgD5vYh9TGgCVZnQ9HxIwoOIQ-IQwFCgYuPDg32SaHAgLUwz47NDw7mYQUWKsxJzwgDU8lGi6WJKQkn8AgBpvTAsIZmsMKCQOECgAUSKR9Ch9AE8ASUxcAHIUCwsodbU-tQBuIA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Creact%2Cstage-2&prettier=false&targets=&version=7.17.2&externalPlugins=&assumptions=%7B%7D
+  <ul>
+    <li>Lunes</li>
+    <li>Martes</li>
+    <li>Miercoles</li>
+    <li>Jueves</li>
+    <li>Viernes</li>
+  </ul>,
   document.getElementById('root'),
 );
 
