0ea55aa159f228f358ab855fa1749590b4b927ac
Kevin Avila <kevin.avila@avantgarde.de>
2022-02-15 10:24:10 +0100

    Custom components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <h1>Kevin's Kitchen</h1>
+    </header>
+  );
+};
+
+function Main() {
+  return (
+    <section>
+      <p>We server the best food in town!</p>
+    </section>
+  );
+};
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Believe it!
+      </p>
+    </footer>
+  );
+};
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 ReactDOM.render(
-  // This is using JSX (JavaScript as XML) this uses Babel to make this work.
-  //https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&corejs=3.6&spec=false&loose=false&code_lz=EoUwhgxgLgIg8gWQHQCcQDsAmIUAoBQABIQPQmEAqAFgJYDOh9hArnTegOaEBSAygBqFc3MADcwvCChoAHKITAN-CADIBKQlFoNWIBgCEwAIxAAbTQHtCAWzABrEJu2EA7hZR2kRQgB5mpgD5vYh9TGgCVZnQ9HxIwoOIQ-IQwFCgYuPDg32SaHAgLUwz47NDw7mYQUWKsxJzwgDU8lGi6WJKQkn8AgBpvTAsIZmsMKCQOECgAUSKR9Ch9AE8ASUxcAHIUCwsodbU-tQBuIA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Creact%2Cstage-2&prettier=false&targets=&version=7.17.2&externalPlugins=&assumptions=%7B%7D
-  <ul>
-    <li>Lunes</li>
-    <li>Martes</li>
-    <li>Miercoles</li>
-    <li>Jueves</li>
-    <li>Viernes</li>
-  </ul>,
+  <App />,
   document.getElementById('root'),
 );
 
