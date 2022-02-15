4189ac18034b7c56a279b88e8714fbbab3a153c9
Kevin Avila <kevin.avila@avantgarde.de>
2022-02-15 16:17:14 +0100

    Working with nested routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import {
-  Home, About, Events, Contact
+  Home,
+  About,
+  Events,
+  Contact,
+  NotFound,
+  Services,
+  CompanyHistory,
+  Location,
 } from './pages/pages';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />}>
+        <Route path="services" element={<Services />} />
+        <Route path="company-history" element={<CompanyHistory />} />
+        <Route path="location" element={<Location />} />
+      </Route>
       <Route path="/events" element={<Events />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link, useLocation, Outlet } from 'react-router-dom';
 
 export function Home() {
   return (
     <>
       <h1>[Company website]</h1>
+      <nav>
+
+        <Link to="about">About</Link>
+        <Link to="about">Events</Link>
+        <Link to="about">Contact</Link>
+      </nav>
     </>
   );
 };
@@ -12,7 +19,32 @@ export function About() {
   return (
     <>
       <h1>[About]</h1>
+      <Outlet />
     </>
+  );
+};
+
+export function Services() {
+  return (
+    <div>
+      <h1>[Services]</h1>
+    </div>
+  );
+};
+
+export function CompanyHistory() {
+  return (
+    <div>
+      <h1>[Company History]</h1>
+    </div>
+  );
+};
+
+export function Location() {
+  return (
+    <div>
+      <h1>[Location]</h1>
+    </div>
   );
 };
 
@@ -28,6 +60,16 @@ export function Contact() {
   return (
     <>
       <h1>[Contact]</h1>
+    </>
+  );
+};
+
+export function NotFound() {
+  const location = useLocation();
+  return (
+    <>
+      <h1>[404 - Not Found!]</h1>
+      <p>Resource not found @{location.pathname}</p>
     </>
   );
 };
